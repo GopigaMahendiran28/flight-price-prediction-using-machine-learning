53314a71153da156dbcb3aa80d44b7596fde6616
GopigaMahendiran28 <gopigamahendiran@gmail.com>
2026-04-26 17:32:46 +0530

    app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def predict():
     return render_template("index.html", prediction_text="Predicted Price: {}".format(prediction[0]))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
